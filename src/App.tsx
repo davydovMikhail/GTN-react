9ac3ab2647490b6eba494b7c7c2af00da0a3f904
davydovMikhail <davydov27977@gmail.com>
2023-08-03 15:12:16 +0300

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Main from './pages/main';
 import Loader from './components/loader/loader';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
       <Main />
       <Loader />
+      <ToastContainer/>
     </>
   );
 }
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,28 +1,125 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { useEthers } from "@usedapp/core";
+import { useGetBalance } from '../hooks/useGetBalance';
+import { useClaimTestTokens } from '../hooks/useClaimTestTokens';
+import { useGetAllowance } from '../hooks/useGetAllowance';
+import { toast } from "react-toastify";
+import { useActions } from '../hooks/useActions';
+import { useApproveToGame } from '../hooks/useApproveToGame';
+import { usePlayBid } from '../hooks/usePlayBid';
+import { useRequestGameHash } from '../hooks/useRequestGameHash';
+import { useGetRandomNumber } from '../hooks/useGetRandomNumber';
+import { useGetLastHash } from '../hooks/useGetLastHash';
+import { useGetCurrentBlockNumber } from '../hooks/useGetBlockNumber';
+import SetInterval from 'set-interval'
 
 const Main = () => {
+    const { activateBrowserWallet, account } = useEthers();
+    const { SetNotification, SetLoader, SetShowOk } = useActions();
+
+    const start: any = useRef();
+
     const [amount, setAmount] = useState('1');
     const [percent, setPercent] = useState('5');
+    const [balance, setBalance] = useState(0);
+    const firstIteration = useRef(true);
+    const getBalanceHook = useGetBalance();
+    const claimHook = useClaimTestTokens();
+    const allowanceHook = useGetAllowance();
+    const approveHook = useApproveToGame();
+    const requestHook = useRequestGameHash();
+    const randomHook = useGetRandomNumber();
+    const playHook = usePlayBid();
+    const hashHook = useGetLastHash();
+    const blockHook = useGetCurrentBlockNumber();
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const balanceAccount = await getBalanceHook(account as string);               
+            setBalance(balanceAccount as number);
+        }
+        fetchData().catch(console.error);
+    }, [account]);
+    async function handlePlay(isGreater: boolean) {
+        if (!account) {
+            toast.info('First connect your wallet', {
+                position: "top-center",
+                autoClose: 1000,
+                hideProgressBar: true,
+                pauseOnHover: false,
+                draggable: true,
+                theme: "colored",
+            });
+            return;
+        }
+        firstIteration.current = true;
+        SetLoader(true);
+        if((await allowanceHook(account) as number) < Number(amount)) {
+            SetNotification('Approve your game tokens to the address of the game contract');
+            await approveHook();
+        }
+        SetNotification('Requesting the hash of your game...');
+        const hashBefore = await hashHook(account);
+        const balanceBefore = (await getBalanceHook(account as string)) as number;
+        const targetBlock = (await requestHook(amount, percent, isGreater))?.blockNumber.toString() as string;
+        SetNotification('Confirm the call to the play function');
+        SetInterval.start(async () => {
+            const currentBlock = (await blockHook()) as number;
+            const hashAfter = await hashHook(account);
+            if(hashBefore !== hashAfter && currentBlock > Number(targetBlock) && firstIteration.current) {
+                firstIteration.current = false;
+                SetInterval.clear('checkHash')
+                clearInterval(start.current);
+                await playHook();
+                const randomNumber = await randomHook(targetBlock, account);
+                const balanceAfter = (await getBalanceHook(account as string)) as number;
+                if(balanceAfter > balanceBefore) {
+                    SetNotification(`You've won! The block number - ${targetBlock}. Random number - ${randomNumber}`);
+                } else {
+                    SetNotification(`Your bet is not played! The block number - ${targetBlock}. Random number - ${randomNumber}`);
+                }
+                SetShowOk(true);     
+                setBalance(balanceAfter);
+            }
+        }, 500, "checkHash")
+    }
+    async function handleClaim() {
+        if (!account) {
+            toast.info('First connect your wallet', {
+                position: "top-center",
+                autoClose: 1000,
+                hideProgressBar: true,
+                pauseOnHover: false,
+                draggable: true,
+                theme: "colored",
+            });
+            return;
+        }
+        SetLoader(true);
+        await claimHook();
+        SetLoader(false);
+        const balanceAccount = await getBalanceHook(account as string);               
+        setBalance(balanceAccount as number);
+    }
     function handleValidateAmount(_amount: string) {
         if (Number(_amount) < 1) {
             setAmount("1");
-        } else if (Number(_amount) > 1000000) {
-            setAmount("1000000");
+        } else if (Number(_amount) > balance) {
+            setAmount(balance.toString());
         } else {
             setAmount(_amount);  
         }
     }
     function handleDoubleAmount() {
         const doubleAmount = Number(amount) * 2;
-        if(doubleAmount > 1000000) {
-            setAmount("1000000");
+        if(doubleAmount > balance) {
+            setAmount(balance.toString());
         } else {
             setAmount(doubleAmount.toString());
         }
     }
     function handleMaxAmount() {
-        setAmount("1000000");
+        setAmount(balance.toString());
     }
     function handleHalfAmount() {
         const halfAmount = Number(amount) / 2;
@@ -148,11 +245,11 @@ const Main = () => {
                                                     <div className="choose">
                                                         <div className="choose__var">
                                                             <div className="choose__diaposon">0-{ getRange() }</div> 
-                                                            <button className="btn btn-warning btn-lg choose__btn">LESS</button>
+                                                            <button onClick={() => handlePlay(false)} className="btn btn-warning btn-lg choose__btn">LESS</button>
                                                         </div>
                                                         <div className="choose__var">
                                                             <div className="choose__diaposon">{ 999999 - getRange() }-999999</div> 
-                                                            <button className="btn btn-warning btn-lg choose__btn">MORE</button>
+                                                            <button onClick={() => handlePlay(true)} className="btn btn-warning btn-lg choose__btn">MORE</button>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -163,12 +260,35 @@ const Main = () => {
                                 </div>
                                 <div className="row">
                                     <div className="col-md-2">
-                                        <button type="button" className="btn btn-warning btn-lg">Connect Wallet</button>
+                                        <button onClick={() => activateBrowserWallet()} type="button" className="btn btn-warning btn-lg">Connect Wallet</button>
                                     </div>
                                     <div className="col-md-2">
-                                        <button type="button" className="btn btn-warning btn-lg">Claim Test Tokens</button>
+                                        <button onClick={() => handleClaim()} type="button" className="btn btn-warning btn-lg">Claim Test Tokens</button>
                                     </div>
                                 </div>
+                                <div className="row">
+                                    <div className="col-md-8 h4" >
+                                        {account ? `Your Wallet: ${account}`: "Click 'Connect Wallet'"}
+                                    </div>
+                                </div>
+                                {account && 
+                                    <div className="row">
+                                        <div className="col-md-8 h4" >
+                                            Your balance: {balance} TGT
+                                        </div>
+                                    </div>
+                                }
+                                <div className="row">
+                                    <div className="col-md-8 h4" >
+                                        Address token: 0x12a804d83957Dd32E7f8bC997681E7Ecd4920949 
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-8 h4" >
+                                        Address game: 0x9E0ab0FE2365c1bF5FB98a7035e49c458CC82C63 
+                                    </div>
+                                </div>
+                            
                                 {/* <div className="row">
                                     <div className="col-md-12">
                                         <h2>Random numbers</h2>
